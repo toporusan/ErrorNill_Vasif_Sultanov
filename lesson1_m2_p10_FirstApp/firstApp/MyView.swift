fb146cf35f3d70426732f4782315d72c343443ca
Vasif <sultanovasif@gmail.com>
2025-03-05 22:54:47 +0500

    s

--- lesson1_m2_p10_FirstApp/firstApp/MyView.swift
+++ lesson1_m2_p10_FirstApp/firstApp/MyView.swift
@@ -7,23 +7,100 @@
 
 import UIKit
 
-class myView: UIViewController {
+class MyView: UIViewController {
+    
+    let face = UIView()
+    let leftEar = UIView()
+    let rightEar = UIView()
+    let leftEye = UIView()
+    let rightEye = UIView()
+    let nose = UIView()
+    let mouth = UIView()
+    let tongue = UIView()
+    let pushMeBTN: UIButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
 
-        // Do any additional setup after loading the view.
+        // Face
+        face.frame = CGRect(x: 50, y: 200, width: 300, height: 300)
+        face.backgroundColor = .brown
+        face.layer.cornerRadius = 150
+        face.layer.borderWidth = 3
+        face.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(face)
+
+        // Left Ear
+        leftEar.frame = CGRect(x: 40, y: 140, width: 100, height: 120)
+        leftEar.backgroundColor = .darkGray
+        leftEar.layer.cornerRadius = 50
+        leftEar.layer.borderWidth = 3
+        leftEar.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(leftEar)
+
+        // Right Ear
+        rightEar.frame = CGRect(x: 260, y: 140, width: 100, height: 120)
+        rightEar.backgroundColor = .darkGray
+        rightEar.layer.cornerRadius = 50
+        rightEar.layer.borderWidth = 3
+        rightEar.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(rightEar)
+
+        // Left Eye
+        leftEye.frame = CGRect(x: 100, y: 80, width: 40, height: 40)
+        leftEye.backgroundColor = .black
+        leftEye.layer.cornerRadius = 20
+        face.addSubview(leftEye)
+
+        // Right Eye
+        rightEye.frame = CGRect(x: 180, y: 80, width: 40, height: 40)
+        rightEye.backgroundColor = .black
+        rightEye.layer.cornerRadius = 20
+        face.addSubview(rightEye)
+
+        // Nose
+        nose.frame = CGRect(x: 130, y: 130, width: 60, height: 40)
+        nose.backgroundColor = .black
+        nose.layer.cornerRadius = 20
+        face.addSubview(nose)
+
+        // Mouth
+        mouth.frame = CGRect(x: 100, y: 170, width: 120, height: 30)
+        mouth.backgroundColor = .black
+        mouth.layer.cornerRadius = 15
+        face.addSubview(mouth)
+
+        // Tongue
+        tongue.frame = CGRect(x: 140, y: 200, width: 40, height: 80)
+        // tongue.backgroundColor = .red
+        tongue.layer.cornerRadius = 30
+        face.addSubview(tongue)
+        
+        pushMeBTN.frame = CGRect(x: 100, y: 550, width: 200, height: 50)
+        pushMeBTN.setTitle("Нажми меня", for: .normal)
+        pushMeBTN.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        pushMeBTN.backgroundColor = .systemBlue
+        pushMeBTN.setTitleColor(.white, for: .normal)
+        pushMeBTN.layer.cornerRadius = 10
+        
+        pushMeBTN.addAction(UIAction { _ in
+            self.animate()
+        }, for: .touchUpInside)
+
+        view.addSubview(pushMeBTN)
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func animate() {
+        UIView.animate(withDuration: 1) {
+            self.tongue.frame = CGRect(x: 140, y: 200, width: 40, height: 80)
+            self.tongue.backgroundColor = .red
+            self.pushMeBTN.backgroundColor = .systemGreen
+            
+            self.pushMeBTN.setTitle("ХаХаХа!!", for: .normal)
+        }
     }
-    */
-
+    
 }
