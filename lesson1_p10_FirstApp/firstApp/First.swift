f7c187eaec2422208502670afa62340e971ee43c
Vasif <sultanovasif@gmail.com>
2025-03-05 20:29:14 +0500

    s

---
+++ lesson1_p10_FirstApp/firstApp/First.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+
+class First: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+
+        
+    }
+    
+
+}
---
+++ lesson1_p10_FirstApp/firstApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Toporusan on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let but: UIButton = UIButton()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Hello World!")
+        
+       // view  это холст для элементов
+        view.backgroundColor = .systemBlue
+        view.tintColor = .systemRed
+        
+        but.setTitle("Hello", for: .normal)
+        
+        but.titleLabel?.font = .systemFont(ofSize: 30)
+        but.frame = CGRect(x: 50, y: 200, width: 200, height: 50)
+        
+        view.addSubview(but)
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
